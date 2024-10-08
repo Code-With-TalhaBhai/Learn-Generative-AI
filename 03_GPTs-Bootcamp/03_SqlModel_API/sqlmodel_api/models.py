2f2aafc3e68b5e8f4d4815774b0c77abb50d9796
= <=>
2024-10-08 11:59:23 +0500

    starting kafka

--- 03_GPTs-Bootcamp/03_SqlModel_API/sqlmodel_api/models.py
+++ 03_GPTs-Bootcamp/03_SqlModel_API/sqlmodel_api/models.py
@@ -5,5 +5,6 @@ from sqlmodel import SQLModel,Field
 class Todo(SQLModel,table=True):
     id: int | None = Field(default=None,primary_key=True)
     title: str = Field(index=True)
+    # title: str = Field(sa_column=Column())
     description: str
     author: str | None = Field(default=None)
