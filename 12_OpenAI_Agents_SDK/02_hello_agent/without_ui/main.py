8c5a426a2a39b5e966b3497299efb5e0466dde06
Code-With-TalhaBhai <talhakhalid411@gmail.com>
2025-10-19 12:50:07 +0500

    done with all removing secrets issues

--- 12_OpenAI_Agents_SDK/02_hello_agent/without_ui/main.py
+++ 12_OpenAI_Agents_SDK/02_hello_agent/without_ui/main.py
@@ -9,7 +9,7 @@ import asyncio
 load_dotenv()
 
 api_key = os.getenv('GEMINI_API_KEY')
-print(api_key)
+# print(api_key)
 
 provider = AsyncOpenAI(
     api_key=api_key,
@@ -34,15 +34,15 @@ async def main():
     agent = Agent(name="Assistant", instructions="You are helpful assistant", model= 'gemini-2.5-flash')
 
     # Without Streaming
-    # result = await Runner.run(agent, 'Write a haiku about recurrsion', run_config=config)
-    # print(result.final_output)
+    result = await Runner.run(agent, 'Write a haiku about recurrsion', run_config=config)
+    print(result.final_output)
 
     # With Streaming
-    result = Runner.run_streamed(agent, 'Write a haiku about recurrsion', run_config=config)
-    print(result)
-    print()
-    async for e in result.stream_events():
-        print(e,"|")
+    # result = Runner.run_streamed(agent, 'Write a haiku about recurrsion', run_config=config)
+    # print(result)
+    # print()
+    # async for e in result.stream_events():
+    #     print(e,"|")
 
 
 
