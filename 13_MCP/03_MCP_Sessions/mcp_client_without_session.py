6ae4c0914c69c1a8e966138a8793577294383225
Code-With-TalhaBhai <talhakhalid411@gmail.com>
2026-03-11 15:13:09 +0500

    mcp tools resources prompts

--- 13_MCP/03_MCP_Sessions/mcp_client_without_session.py
+++ 13_MCP/03_MCP_Sessions/mcp_client_without_session.py
@@ -5,33 +5,130 @@ headers = {
     "Accept": "application/json,text/event-stream"
 }
 
-
-# Resources
-
-# List Resources
-# resouces_list = requests.post(url='http://localhost:8000/mcp/',headers=headers,json={
-#     "jsonrpc": "2.0",
-#     "id": 2,
-#     "method": "resources/list",
-#     "params": {}
-# })
-# print(resouces_list.text)
+url = "http://localhost:8000/mcp/"
 
 
-# Read Resources
-read_resouces1= requests.post(url='http://localhost:8000/mcp/',headers=headers,json={
+
+# ---------------------------------------- Tools -------------------------------------
+
+
+
+# List Tools
+tools_list = requests.post(url=url,headers=headers,json={
+    "jsonrpc": "2.0",
+    "id": 2,
+    "method": "tools/list",
+    "params": {}
+})
+print('Tools List')
+print(tools_list.text)
+
+
+
+# Call Tools
+tool_call_1 = requests.post(url, headers=headers, json={
+    "jsonrpc": "2.0",
+    "id": 2,
+    "method": "tools/call",
+    "params": {"name": "get_docs", "arguments": {"doc_id": "report.pdf"}}
+})
+print('Tools Call: 1')
+print(tool_call_1.text)
+
+
+tool_call_2 = requests.post(url, headers=headers, json={
+    "jsonrpc": "2.0",
+    "id": 2,
+    "method": "tools/call",
+    "params": {"name": "edit_docs", "arguments": {"doc_id": "report.pdf", "new_str": "This is the new report added to pdf"}}
+})
+print('Tools Call: 2')
+print(tool_call_2.text)
+
+
+# Again making tool call to see if there actual changes happened in 'report.pdf'
+tool_call_3 = requests.post(url, headers=headers, json={
+    "jsonrpc": "2.0",
+    "id": 2,
+    "method": "tools/call",
+    "params": {"name": "get_docs", "arguments": {"doc_id": "report.pdf"}}
+})
+print('Tools Call: 3')
+print(tool_call_3.text)
+
+
+# ---------------------------------------- RESOURCES -------------------------------------
+
+# # List Resources
+resouces_list = requests.post(url=url,headers=headers,json={
+    "jsonrpc": "2.0",
+    "id": 2,
+    "method": "resources/list",
+    "params": {}
+})
+print('List Resources')
+print(resouces_list.text)
+
+
+# Read Particular Resources
+read_resouces1= requests.post(url=url,headers=headers,json={
     "jsonrpc": "2.0",
     "id": 2,
     "method": "resources/read",
     "params": {"uri": "docs://documents"}
 })
+print('Resources 1')
 print(read_resouces1.text)
 
-
-read_resouces2= requests.post(url='http://localhost:8000/mcp/',headers=headers,json={
+read_resouces2= requests.post(url=url,headers=headers,json={
     "jsonrpc": "2.0",
     "id": 2,
     "method": "resources/read",
-    "params": {"uri": "docs://{financials.docx}"}
+    "params": {"uri": "docs://documents/financials.docx"}
 })
-print(read_resouces1.text)
+print('Resources 2')
+print(read_resouces2.text)
+
+read_resouces3= requests.post(url=url,headers=headers,json={
+    "jsonrpc": "2.0",
+    "id": 2,
+    "method": "resources/read",
+    "params": {"uri": "docs://documents/outlook.pdf"}
+})
+print('Resources 3')
+print(read_resouces3.text)
+
+
+
+# ------------------------------------- PROMPTS -----------------------------------------
+
+# List Prompts
+prompts_list = requests.post(url=url,headers=headers,json={
+    "jsonrpc": "2.0",
+    "id": 2,
+    "method": "prompts/list",
+    "params": {}
+})
+print('List Prompts')
+print(prompts_list.text)
+
+
+# Read Prompts
+prompt1 = requests.post(url=url,headers=headers,json={
+    "jsonrpc": "2.0",
+    "id": 2,
+    "method": "prompts/get",
+    "params": {"name": "format","arguments": {"doc_id": "spec.txt"}}
+})
+print('Prompt 1')
+print(prompt1.text)
+
+
+prompt2 = requests.post(url=url,headers=headers,json={
+    "jsonrpc": "2.0",
+    "id": 2,
+    "method": "prompts/get",
+    "params": {"name": "summarize","arguments": {"doc_id": "spec.txt"}}
+})
+print('Prompt 2')
+print(prompt2.text)
--- 13_MCP/03_MCP_Sessions/mcp_server.py
+++ 13_MCP/03_MCP_Sessions/mcp_server.py
@@ -1,4 +1,6 @@
 from mcp.server.fastmcp import FastMCP
+from mcp.server.fastmcp.prompts import base
+from mcp.types import PromptMessage, TextContent
 
 
 mcp = FastMCP(name="MCP Tool Server", log_level="ERROR", stateless_http=True)
@@ -27,23 +29,53 @@ def edit_docs(doc_id:str, new_str:str):
     return new_str
 
 
-
+# TODO: Write a resource to return all doc id's
 @mcp.resource(uri='docs://documents',mime_type="application/json")
-def list_docs():
+def list_docs_resources():
     return list(docs.keys())
 
 
-
+# Resource template
+# TODO: Write a resource to return the contents of a particular doc
 @mcp.resource(uri="docs://documents/{doc_id}", mime_type="application/json")
 def doc_content(doc_id: str):
     if doc_id in docs:
         return docs[doc_id]
     else:
-        raise mcp.ResourceNotFound('Document Not Found')
+        # raise mcp.ResourceNotFound('Document Not Found')
+        raise Exception('Document Not Found')
 
 
 
+# TODO: Write a prompt to rewrite a doc in markdown format
+# @mcp.prompt()
+@mcp.prompt(name="format", description="Rewrites the contents of the document in Markdown format")
+def format_document(doc_id: str)->list[base.Message]:
+    prompt = f"""
+    Your goal is to reformat a document to be written with markdown syntax.
 
+    The contents of the document you need to reformat is:
+    <document_content>
+    {docs[doc_id]}
+    </document_content>
+
+    Add in headers, bullet points, tables, etc as necessary. Feel free to add in extra text, but don't change the meaning of the report.
+    After the document has been edited, respond with the final version of the doc. Don't explain your changes.
+    """
+
+    return [base.UserMessage(prompt)]
+
+
+
+@mcp.prompt(name="summarize", description="Summarizes the contents of the document")
+def summarize_document(doc_id: str ) -> list[PromptMessage]:
+
+    prompt_text = f"""
+    Your goal is to summarize the contents of the document.
+    Document Contents: {docs[doc_id]}
+    Include a concise summary of the document's main points.
+    """
+    return [PromptMessage(role="user", content=TextContent(type="text", text=prompt_text))]
 
 
 
